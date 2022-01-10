6a034cbff9f3633322aadd38d484461efae031a3
bcatouillard <catouillard.benjamin@gmail.com>
2022-01-10 17:06:40 +0100

    feat(server): add cors policy

--- bin/server.ts
+++ bin/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
-import { urlencoded, json } from 'body-parser';
+import { json } from 'body-parser';
 import morgan from 'morgan';
 import connectToDb  from './database';
 import register from './routes';
@@ -13,9 +14,22 @@ app.use(urlencoded({
    extended: true
 }));
 
+const whitelistedAddress = [
+   'http://localhost:3000'
+];
+
+const corsOptions = {
+   origin: (origin: any, callback: any) => {
+      const isWhitelisted = whitelistedAddress.indexOf(origin) !== -1
+      callback(null, isWhitelisted)
+   },
+   credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(json());
 app.use(morgan(':method :url :status - :response-time ms'));
-app.use(express.urlencoded({ extended: true }));
+app.use(urlencoded({ extended: true }));
 
 app.listen(process.env.PORT, () => {
     // eslint-disable-next-line
