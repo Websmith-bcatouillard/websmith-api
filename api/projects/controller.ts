7492566ad4c7f9c2856046f2fc873f47fa917f82
bcatouillard <catouillard.benjamin@gmail.com>
2022-01-10 16:52:50 +0100

    feat(project): add condition if project is already created

--- api/projects/controller.ts
+++ api/projects/controller.ts
@@ -1,9 +1,27 @@
+import { Request, Response } from 'express';
 import SuperController from '../../utils/superController';
 import projectModel from './model';
 class ProjectController extends SuperController {
 
    constructor(model = projectModel) {
       super(model)
+   }
+
+   async create(req: Request, res: Response) {
+      const { name } = req.body;
+
+      try {
+         const isProjectAlreadyIn = await projectModel.find({ name: name});
+
+         if(isProjectAlreadyIn){
+            res.status(403).json({ success: false, message: 'Project is already saved'});
+            return;
+         }
+
+         super.create(req, res);
+      } catch (error) {
+         res.status(500).json({ success: false, message: error});
+      }
    }
 }
 
