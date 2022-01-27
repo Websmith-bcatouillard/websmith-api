a085e863141914c5cee72ef625fcb4b374b44367
Benjamin Catouillard <catouillard.benjamin@gmail.com>
2022-01-27 11:51:02 +0100

    doc(userSwagger): add swagger for User Endpoints

--- api/users/route.ts
+++ api/users/route.ts
@@ -4,16 +4,97 @@ import UserController from './controller';
 const userController = new UserController();
 const router = Router();
 
+/**
+ * @openapi
+ * /users/:
+ *   post:
+ *     tags:
+ *       - "user"
+ *     summary: Register a new user
+ *     operationId: register 
+ *     description: Register new user
+ *     parameters:
+ *     - in: "body"
+ *       name: "body"
+ *       description: User object
+ *       required: true
+ *       schema:
+ *         $ref: "#/definitions/User"  
+ *     responses:
+ *       200:
+ *         description: Returns a success message.
+ *       403:
+ *         description: User is already registered.
+ *       500:
+ *         description: Returns an error message.
+ *       501:
+ *         description: Returns a salt password error message.
+ *       502:
+ *         description: Returns a hash password error message.
+ */
 router.route('/')
    .post((req: Request, res: Response) => {
       userController.create(req, res);
    })
 
+
+/**
+ * @openapi
+ * /users/:id:
+ *   put:
+ *     tags:
+ *       - "user"
+ *     summary: Update an user
+ *     operationId: updateUser 
+ *     description: Update existing user
+ *     parameters:
+ *       - in: "query"
+ *         name: "ID"
+ *         description: User ID
+ *         type: string
+ *         required: true
+ *       - in: "body"
+ *         name: "body"
+ *         description: User object
+ *         required: true
+ *         schema:
+ *           $ref: "#/definitions/User"  
+ *     responses:
+ *       200:
+ *         description: Returns a success message.
+ *       500:
+ *         description: Returns an error message.
+ */
 router.route('/:id')
    .put((req: Request, res: Response) => {
       userController.update(req, res);
    })
 
+
+/**
+ * @openapi
+ * /users/login:
+ *   post:
+ *     tags:
+ *       - "user"
+ *     summary: Login 
+ *     operationId: login 
+ *     description: login 
+ *     parameters:
+ *     - in: "body"
+ *       name: "body"
+ *       description: User object
+ *       required: true
+ *       schema:
+ *         $ref: "#/definitions/User"  
+ *     responses:
+ *       200:
+ *         description: Returns a success message & an authorization token.
+ *       404:
+ *         description: User not found or password incorrect.
+ *       500:
+ *         description: Returns an error message.
+ */
 router.route('/login')
    .post((req: Request, res: Response) => {
       userController.login(req, res)
--- bin/routes.ts
+++ bin/routes.ts
@@ -1,9 +1,54 @@
 import { Application } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 import userRoutes from '../api/users/route';
 import projectRoutes from '../api/projects/route';
 import { verifyMiddleware } from '../utils/authToken';
 
+const swaggerOptions = {
+   swaggerDefinition: {
+      info: {
+         version: "1.0.0",
+         title: "Websmith API",
+         description: "Websmith API Documentation",
+         contact: {
+            name: "Benjamin Catouillard"
+         },
+         servers: ["http://localhost:5000"]
+      },
+      tags: [
+         {
+            "name": "user",
+            "description": "User Endpoints",
+         },
+         {
+            "name": "project",
+            "description": "Project Endpoints"
+         },
+      ],
+      definitions: {
+         User: {
+            type: "object",
+            properties: {
+               username: {
+                  type: "string",
+               },
+               password: {
+                  type: "string",
+               }
+            },
+         }
+      },
+   },
+   // ['.routes/*.js']
+   apis: ["./api/**/route.ts"]
+};
+
 const register = (app: Application) => {
+
+   const swaggerDocs = swaggerJSDoc(swaggerOptions);
+   app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
    app.use('/users', userRoutes);
 
    // Use Middleware only for Projects Routes
